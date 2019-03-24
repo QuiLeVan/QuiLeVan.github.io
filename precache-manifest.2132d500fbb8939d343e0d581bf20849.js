2c1fbd825e07dd6ffa4b37c82d7709d8309fb111
Qui Le <levanqui88@gmail.com>
2019-03-25 06:32:26 +0700

    * add to test

---
+++ precache-manifest.2132d500fbb8939d343e0d581bf20849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7f8c23be079179a9196",
+    "url": "/static/css/main.8f5855ae.chunk.css"
+  },
+  {
+    "revision": "f7f8c23be079179a9196",
+    "url": "/static/js/main.f7f8c23b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aabb89d05f568f9f1672c93968f238ca",
+    "url": "/index.html"
+  }
+];
